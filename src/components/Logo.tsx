67b4f3c48bbfdf87c79dfe76098c415e47341377
Doran <santiagocubillosarana@gmail.com>
2025-05-23 12:41:31 -0500

    Fix unused logo variable in Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,11 +3,18 @@ import { useLogo } from '../contexts/LogoContext';
 
 const Logo: React.FC<{ className?: string }> = ({ className = '' }) => {
   const { logo } = useLogo();
-  const uscisLogo = '/home/santiago/Descargas/project-bolt-github-1wam1vwm/project/WhatsApp Image 2025-05-22 at 8.17.29 AM(1).png';
-
+  
   return (
     <div className={`flex items-center ${className}`}>
-      <img src={uscisLogo} alt="USCIS Logo" className="h-12 w-auto" />
+      {logo ? (
+        <img src={logo} alt="Company Logo" className="h-12 w-auto" />
+      ) : (
+        <img 
+          src="https://images.pexels.com/photos/20787/pexels-photo.jpg"
+          alt="Default Logo" 
+          className="h-12 w-auto" 
+        />
+      )}
     </div>
   );
 };
