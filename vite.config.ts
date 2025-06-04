fde9ac1f1d23d4a156336acdc790a69aeeb4e87e
Doran <santiagocubillosarana@gmail.com>
2025-06-04 14:34:36 -0500

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: 'all',
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
