c2dd8bc9efbb64341fbe41b4ba46ae7ef86d3a1d
Doran <santiagocubillosarana@gmail.com>
2025-06-04 13:51:02 -0500

    Update Vite configuration for proper SSL

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'node:fs';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +11,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    https: true,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '.cert/key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '.cert/cert.pem')),
+    },
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
