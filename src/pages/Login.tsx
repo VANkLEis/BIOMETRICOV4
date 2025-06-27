527cb71219bca6bc85118771128ecd1bbd707182
Doran <santiagocubillosarana@gmail.com>
2025-06-27 09:11:24 -0500

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { Video, AtSign, Lock } from 'lucide-react';
+import Logo from '../components/Logo';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
